2604e9faea4483679488c3f7b37ed0481f91a2b5
ricguer <129613022+ricguer@users.noreply.github.com>
2023-05-07 19:44:33 -0400

    Add Score Keeping Feature
    - Add score keeping functionality. Score will be updated with each answered question.
    - Load "High Scores" page upon completion of quiz.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,60 @@
+let userScore = 0;
+let questionList = document.getElementsByClassName("question-answer");
+let pointIncrement = (100 / questionList.length);
+let currentQuestionIndex = 0;
 let quizMain = document.getElementById("quiz-main");
 
-quizMain.addEventListener("click", function (event) {
-    let element = event.target;
+try 
+{
+    quizMain.addEventListener("click", function (event) {
+        let element = event.target;
+    
+        if (element.matches("button"))
+        {
+            updateUserScore(element)                                       /* Increment/Decrement User Score                       */
+            goToNextQuestion();
+        }
+    });
+} catch (error) {}
 
-    if (element.matches("button"))
+
+function updateUserScore(element) 
+{
+    let answer = element.getAttribute("data-answer");
+    let resultNotice = document.getElementById("answerResult");
+
+                                                                /* Determine whether user answer is "right" or "wrong"  */
+    if (answer === "Right")
     {
-        let answer = element.getAttribute("data-answer");
+        userScore += pointIncrement;                            /* Increment user score for "Right" answer              */
+    }
+    else if ((answer === "Wrong") && 
+             (userScore > 0))
+    {
+        userScore -= pointIncrement;                            /* Decrement user score for "Wrong" answr except when 0 */
+    }
+
+    resultNotice.children[0].innerHTML = answer + "!";
+    resultNotice.style.visibility = "visible";
+}
+
+
+function goToNextQuestion()
+{
+    let nextQuestionIndex = currentQuestionIndex + 1;
+
+    questionList[currentQuestionIndex].style.display = "none";  /* Hide currently displayed (answered) question         */
+
+    if (nextQuestionIndex < questionList.length)
+    {
+        questionList[nextQuestionIndex].style.display = "flex";
+        currentQuestionIndex = nextQuestionIndex;
+    }
+    else
+    {
         let resultNotice = document.getElementById("answerResult");
 
-        resultNotice.children[0].innerHTML = answer + "!";
-        console.log(resultNotice.children[0].textContent);
-
-        resultNotice.style.visibility = "visible";
+        resultNotice.style.visibility = "hidden";
+        window.location = "./highscores.html";                  /* Load "High Scores" page after last quest is answered */
     }
-});
+}
